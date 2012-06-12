c7a68fd2aabc21462335ed14c297f848b835226e
Alex Kong <alex.kong91@gmail.com>
2012-06-12 13:01:19 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChineseMeds::Application.config.secret_token = 'a33e7c3c7ce9643f1ea6f0ac537df58ba082b49761a8b4a280e182c87bac633c5953d69efea0eed2348e260f340faad5af174b29b9f08eb81340d1e78736a86e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ChineseMeds::Application.config.session_store :cookie_store, key: '_chinese_meds_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ChineseMeds::Application.config.session_store :active_record_store
