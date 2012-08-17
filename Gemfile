78307549e337feb71315ab3957deb7646a16d3ed
Alec Hoey <alechoey@gmail.com>
2012-08-17 03:09:52 -0700

    Deployment files

--- Gemfile
+++ Gemfile
@@ -30,9 +30,7 @@ group :assets do
   gem 'uglifier', '1.2.3'
 end
 
-gem 'jquery-rails', '2.0.0'
-
 group :production do
   gem 'pg', '0.12.2'
-  gem 'unicorn'
+  gem 'passenger'
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,94 +1,72 @@
-# Sample verbose configuration file for Unicorn (not Rack)
-#
-# This configuration file documents many features of Unicorn
-# that may not be needed for some applications. See
-# http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
-# for a much simpler configuration file.
-#
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
+# default to 1 worker, allowing override by ENV variable
+worker_processes [1, ENV['UNICORN_WORKERS'].to_i].max
 
-# Use at least one worker per core if you're on a dedicated server,
-# more will usually help for _short_ waits on databases/caches.
-worker_processes 4
-
-# Since Unicorn is never exposed to outside clients, it does not need to
-# run on the standard HTTP port (80), there is no reason to start Unicorn
-# as root unless it's from system init scripts.
-# If running the master process as root and the workers as an unprivileged
-# user, do this to switch euid/egid in the workers (also chowns logs):
-# user "unprivileged_user", "unprivileged_group"
-
-# Help ensure your application will always spawn in the symlinked
-# "current" directory that Capistrano sets up.
-working_directory "/home/ubuntu/www/chinese_meds/current" # available in 0.94.0+
-
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen "/tmp/unicorn.sock", :backlog => 64
-listen 3000, :tcp_nopush => true
-
-# nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
-
-# feel free to point this anywhere accessible on the filesystem
-pid "/home/ubuntu/www/chinese_meds/shared/pids/unicorn.pid"
-
-# By default, the Unicorn logger will write to stderr.
-# Additionally, ome applications/frameworks log to stderr or stdout,
-# so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/ubuntu/www/chinese_meds/shared/log/unicorn.stderr.log"
-stdout_path "/home/ubuntu/www/chinese_meds/shared/log/unicorn.stdout.log"
-
-# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+# This loads the application in the master process before forking
+# worker processes
+# Read more about it here:
+# http://unicorn.bogomips.org/Unicorn/Configurator.html
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
-  GC.copy_on_write_friendly = true
 
-before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+# Restart any workers that haven't responded in 60 seconds
+timeout 60
 
-  # The following is only recommended for memory/DB-constrained
-  # installations.  It is not needed if your system can house
-  # twice as many worker_processes as you have configured.
+# This is where we specify the port.
+# We will point the upstream Nginx module to this port later on
+# The :tcp_nopush => false option allows for HTTP streaming, but we shouldn't need it
+# listen '/home/ubuntu/apps/magoosh_app/current/tmp/sockets/unicorn.sock' #, :tcp_nopush => false
+listen 3000 # we can't use sockets because it breaks no-downtime deploys :(
+
+# By default, Unicorn master processes fork to the same directory that they were started in, but
+# since Capistrano cleans up old releases we need to tell 
+# http://unicorn.bogomips.org/Unicorn/Configurator.html#method-i-working_directory
+working_directory '/home/ubuntu/www/chinese_meds/current'
+
+# Set the path of the log files
+stderr_path '/home/ubuntu/www/chinese_meds/shared/log/unicorn.stderr.log'
+stdout_path '/home/ubuntu/www/chinese_meds/shared/log/unicorn.stdout.log'
+
+# Set the path of the PID file
+pid_path = '/home/ubuntu/www/chinese_meds/shared/pids/unicorn.pid'
+
+pid pid_path 
+
+before_exec do |server|
+  ##
+  # Capistrano has strange interactions with the BUNDLE_GEMFILE enivornment
+  # variable, so we have to manually set it every time unicorn restarts :(
   #
-  # # This allows a new master process to incrementally
-  # # phase out the old master process with SIGTTOU to avoid a
-  # # thundering herd (especially in the "preload_app false" case)
-  # # when doing a transparent upgrade.  The last worker spawned
-  # # will then kill off the old master process with a SIGQUIT.
-  # old_pid = "#{server.config[:pid]}.oldbin"
-  # if old_pid != server.pid
-  #   begin
-  #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-  #     Process.kill(sig, File.read(old_pid).to_i)
-  #   rescue Errno::ENOENT, Errno::ESRCH
-  #   end
-  # end
-  #
-  # Throttle the master from forking too quickly by sleeping.  Due
-  # to the implementation of standard Unix signal handlers, this
-  # helps (but does not completely) prevent identical, repeated signals
-  # from being lost when the receiving process is busy.
-  # sleep 1
+  # http://unicorn.bogomips.org/Sandbox.html
+  ENV["BUNDLE_GEMFILE"] = "/home/ubuntu/www/chinese_meds/current/Gemfile"
 end
 
+before_fork do |server, worker|
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+  
+  old_pid = "#{pid_path}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+
 after_fork do |server, worker|
-  # per-process listener ports for debugging/admin/migrations
-  # addr = "127.0.0.1:#{9293 + worker.nr}"
-  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
-
-  # the following is *required* for Rails + "preload_app true",
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
+  ##
+  # Unicorn master loads the app then forks off workers - because of the way
+  # Unix forking works, we need to make sure we aren't using any of the parent's
+  # sockets, e.g. db connection
+  
+  ActiveRecord::Base.establish_connection
 end
