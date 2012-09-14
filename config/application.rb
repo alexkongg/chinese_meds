652ed322aeba4c5e4f14e23bc9060186203af4ec
Alex Kong <alex.kong91@gmail.com>
2012-09-14 03:30:29 -0700

    One more time1

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module ChineseMeds
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.initialize_on_precomiple = false
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
