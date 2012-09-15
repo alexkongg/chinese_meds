6ef13ee393a15dd4c468009610f75d6cd1beb271
Alex Kong <alex.kong91@gmail.com>
2012-09-14 22:49:43 -0700

    Updated Robots.txt

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,6 @@ ChineseMeds::Application.routes.draw do
   match '/signout',                      to: "sessions#destroy", via: :delete
   match '/search',                       to: "herbs#search"
   match '/traditional-chinese-medicine', to: "tcms#index"
-  match '/about',                        to: "static_pages#about"
-
-  match '/contact',                      to: "static_pages#contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
