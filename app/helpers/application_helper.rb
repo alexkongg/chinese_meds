80a7f4c02a9d97980c5680a104542eb2992569e6
Alex Kong <alex.kong91@gmail.com>
2012-11-02 15:05:07 -0700

    fixed input box size on hp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   #returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Chinese-Meds"
+    base_title = "Chinese-Meds.com"
     if page_title.empty?
       base_title
     else
