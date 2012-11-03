a9745953915c1a17ed3c296ba1c2bc6759d323b6
Alex Kong <alex.kong91@gmail.com>
2012-11-03 03:46:37 -0700

    new hp redesign

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,19 @@
 jQuery(function($){
     $(".chosen").chosen({no_results_text: "No results matched"});
 });
+
+CM = {
+  setup: function() {
+    $('#mainPopUp').hide();
+  }
+}
+
+$(CM.setup);
+
+
+$(function() {                         
+  $('#Browse').click(function () {
+    $('#mainPopUp').show('slow');
+    $('#Browse').hide(1200);
+    })
+});
